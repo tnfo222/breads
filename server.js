f3cdc8538b666e7797fa43bdd363c0a67dc09b24
tnfo222 <github.com/tnfo222/psychic-disco>
2022-07-02 19:29:43 -0400

    sending index and show data

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+//Configuration
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+  
+
+//Listen
+app.listen(PORT, () => {
+    console.log('nomming at port', PORT);
+})
+
+//Check Port
+//console.log(PORT)
