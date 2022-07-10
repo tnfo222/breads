57a15d223b4036623239f2f74884609cf1e17c38
tnfo222 <tayforbes92@hotmail.com>
2022-07-10 13:02:40 -0400

    delete route complete

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //Dependencies
 const express = require('express')
+const methodOverride = require('method-override')
+
 
 //Configuration
 require('dotenv').config()
@@ -7,6 +9,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.set('view engine', 'jsx')
