d4d301ec4525fe5defb9647726defd85adb62a5b
tnfo222 <tayforbes92@hotmail.com>
2022-07-12 21:38:52 -0400

    Added valid enum value for path baker

--- models/bread.js
+++ models/bread.js
@@ -6,7 +6,11 @@ const { Schema } = mongoose
 const breadSchema = new Schema({
   name: { type: String, required: true },
   hasGluten: Boolean,
-  image: { type: String, default: '../images/placeholder.png' }
+  image: { type: String, default: 'http://placehold.it/500x500.png' },
+  baker: {
+    type: String,
+    enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+  }
 })
 
 const Bread = mongoose.model('Bread', breadSchema)
