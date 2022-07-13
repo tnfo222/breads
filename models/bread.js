843d6bdea3898f39dd109b47cac2b6c1983fc105
tnfo222 <tayforbes92@hotmail.com>
2022-07-12 21:57:57 -0400

    Added baked by sentence to old and new breads

--- models/bread.js
+++ models/bread.js
@@ -13,6 +13,11 @@ const breadSchema = new Schema({
   }
 })
 
+// helper methods 
+breadSchema.methods.getBakedBy = function(){
+  return `${this.name} was baked with love by ${this.baker}`
+}
+
 const Bread = mongoose.model('Bread', breadSchema)
 
 module.exports = Bread
