325e6ada56da7bdcbd82de89f1bb31cbea7e0196
tnfo222 <tayforbes92@hotmail.com>
2022-07-12 21:48:27 -0400

    Added bakers to old and new fields

--- views/edit.jsx
+++ views/edit.jsx
@@ -6,30 +6,40 @@ function Edit ({bread, index}) {
       <Default>
         <h2>Edit a bread</h2>
         <form action={`/breads/${bread.id}?_method=PUT`} method="POST">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            required
-            defaultValue={bread.name}
-          />
-          <label htmlFor="image">Image</label>
-          <input
-            type="text"
-            name="image"
-            id="image"
-            defaultValue={bread.image}
-          />
-          <label htmlFor="hasGluten">Has Gluten?</label>
-          <input
-            type="checkbox"
-            name="hasGluten"
-            id="hasGluten"
-            defaultChecked={bread.hasGluten}
-          />
-          <br />
-          <input type="submit"/>
+            <label htmlFor="name">Name</label>
+            <input
+                type="text"
+                name="name"
+                id="name"
+                required
+                defaultValue={bread.name}
+            />
+            <label htmlFor="image">Image</label>
+            <input
+                type="text"
+                name="image"
+                id="image"
+                defaultValue={bread.image}
+            />
+            <label htmlFor="baker">Baker</label>
+            <select name="baker" id="baker" defaultValue={bread.baker}>
+                <option value="Rachel">Rachel</option>
+                <option value="Monica">Monica</option>
+                <option value="Joey">Joey</option>
+                <option value="Chandler">Chandler</option>
+                <option value="Ross">Ross</option>
+                <option value="Phoebe">Phoebe</option>
+            </select>
+
+            <label htmlFor="hasGluten">Has Gluten?</label>
+            <input
+                type="checkbox"
+                name="hasGluten"
+                id="hasGluten"
+                defaultChecked={bread.hasGluten}
+            />
+            <br />
+            <input type="submit"/>
         </form>
       </Default>
     )
