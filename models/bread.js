857cbc4e5c8150513caa5cdb1d0f66f9aecb4e5b
tnfo222 <tayforbes92@hotmail.com>
2022-07-12 21:42:14 -0400

    Fixed random image placeholder in bread model

--- models/bread.js
+++ models/bread.js
@@ -6,7 +6,7 @@ const { Schema } = mongoose
 const breadSchema = new Schema({
   name: { type: String, required: true },
   hasGluten: Boolean,
-  image: { type: String, default: 'http://placehold.it/500x500.png' },
+  image: { type: String, default: '../images/placeholder.png' },
   baker: {
     type: String,
     enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
