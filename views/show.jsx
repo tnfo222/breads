081124759fba10740146217eafac79ab87516b4b
tnfo222 <tayforbes92@hotmail.com>
2022-07-14 20:49:18 -0400

    Added Bakers

--- views/show.jsx
+++ views/show.jsx
@@ -17,7 +17,8 @@ function Show ({bread,index}) {
           have gluten.
         </p>
         <img src={bread.image} alt={bread.name} />
-        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+        <p>{bread.getBakedBy()}</p>
+        <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
         <form action={`/breads/${index}?_method=DELETE`} method="POST">
           <input type='submit' value="DELETE"/>
         </form>
